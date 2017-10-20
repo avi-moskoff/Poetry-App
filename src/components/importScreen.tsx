a3f07d70ccc08e875adcc6f0cca20cbe4f9024d6
Avi Moskoff <pireads@gmail.com>
2017-10-20 12:17:00 -0600

    Code style updates :P

--- src/components/importScreen.tsx
+++ src/components/importScreen.tsx
@@ -4,49 +4,52 @@ import { KeyboardAvoidingView, Button, TextInput } from 'react-native'
 import styles from '../styles'
 
 interface IProps extends React.Props<void> {
-    navigation: { navigate: (screen: string, props?: React.Props<void>) => void }
+	navigation: { navigate: (screen: string, props?: React.Props<void>) => void }
 }
 
 class ImportScreen extends React.Component<IProps> {
 
-    static navigationOptions = (navigation: { navigate: (screen: string, props?: React.Props<void>) => void }) => ({
-        title: 'ImportScreen',
-        headerRight: <Button title='wat' onPress={() => {
-            navigation.navigate('EditScreen')
-        }}/>
-    })
+	static navigationOptions = (navigation: { navigate: (screen: string, props?: React.Props<void>) => void }) => ({
+		title: 'ImportScreen',
+		headerRight: <Button
+			title='wat'
+			onPress={() => {
+				navigation.navigate('EditScreen')
+			}}
+		/>
+	})
 
-    private _poem: string
+	private _poem: string
 
-    constructor(props: IProps) {
-        super(props)
-        this.state = {
-            poem: ''
-        }
-    }
+	constructor(props: IProps) {
+		super(props)
+		this.state = {
+			poem: ''
+		}
+	}
 
-    render(): JSX.Element {
-        console.log(this._poem)
-        return (
-            <KeyboardAvoidingView   
-                behavior='padding' 
-                style={styles.importScreen}
-            >
-                <TextInput 
-                    style={styles.normalText} 
-                    placeholderTextColor='#ecf0f1'
-                    placeholder='Type your poem here!' 
-                    keyboardAppearance='dark'
-                    multiline={true}
-                    autoFocus={true}
-                    onChangeText={(poem: string) => {
-                        this.setState({poem})
-                        this._poem = poem
-                    }} 
-                />
-            </KeyboardAvoidingView>
-        )
-    }
+	render(): JSX.Element {
+		console.log(this._poem)
+		return (
+			<KeyboardAvoidingView
+				behavior='padding'
+				style={styles.importScreen}
+			>
+				<TextInput
+					style={styles.normalText}
+					placeholderTextColor='#ecf0f1'
+					placeholder='Type your poem here!'
+					keyboardAppearance='dark'
+					multiline={true}
+					autoFocus={true}
+					onChangeText={(poem: string) => {
+						this.setState({ poem })
+						this._poem = poem
+					}}
+				/>
+			</KeyboardAvoidingView>
+		)
+	}
 }
 
 export default ImportScreen
--- src/styles.ts
+++ src/styles.ts
@@ -1,35 +1,35 @@
-import {StyleSheet} from 'react-native'
+import { StyleSheet } from 'react-native'
 
 const styles: any = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#2c3e50',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-    },
-    importScreen: {
-      flex: 1,
-      // paddingBottom: 50,
-      backgroundColor: '#2c3e50'
-    },
-    titleView: {
-      flex: 0.4,
-      backgroundColor: '#2c3e50',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    title: {
-      fontSize: 100,
-      color: '#ecf0f1'
-    },
-    normalText: {
-      fontSize: 30,
-      color: '#ecf0f1',
-      padding: 10
-    },
-    button: {
-      color: '#ecf0f1'
-    }
-  })
+	container: {
+		flex: 1,
+		backgroundColor: '#2c3e50',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	},
+	importScreen: {
+		flex: 1,
+		// paddingBottom: 50,
+		backgroundColor: '#2c3e50'
+	},
+	titleView: {
+		flex: 0.4,
+		backgroundColor: '#2c3e50',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	title: {
+		fontSize: 100,
+		color: '#ecf0f1'
+	},
+	normalText: {
+		fontSize: 30,
+		color: '#ecf0f1',
+		padding: 10
+	},
+	button: {
+		color: '#ecf0f1'
+	}
+})
 
 export default styles
