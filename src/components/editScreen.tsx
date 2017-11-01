f4edfd8393e05f2a4fa3a1bbba790aa8658086a9
Joe Archibold <joearchibold@Teresas-iMac.local>
2017-11-01 11:51:02 -0600

    Closer to the end.

--- src/components/editScreen.tsx
+++ src/components/editScreen.tsx
@@ -12,23 +12,47 @@ class EditScreen extends React.Component<IProps> {
 		title: 'EditScreen'
 	}
 
+	private synonymMap: {
+		[index: string] : string[]
+	}
+
 	componentWillMount(): void {
 		const poem: string = this.props.navigation.state.params.poem
 
 		const poemArray: string[] = poem.split(/[ \n\r\t]/g)
 
-		fetch('http://thesaurus.altervista.org/thesaurus/v1?key=bD9XnankasfsfRsNcn2c&word=strategy&language=en_US&output=json').catch((error: any) => {
-			console.log(error)
-		}).then((response: Response) => {
-			return response.json()
-		}).then((response: IResponse) => {
-
-			const responseArray: [IList] = response.response
-
-			console.log(response.response[0].list.synonyms)
+		poemArray.forEach((word) => {
+			fetch(`http://thesaurus.altervista.org/thesaurus/v1?key=bD9XnankasfsfRsNcn2c&language=en_US&output=json&word=${word}`).catch((error: any) => {
+				console.log(error)
+			}).then((response: Response) => {
+				if(response.ok) {
+					return response.json()
+				} else {
+					return {
+						response: []
+					}
+				}
+			}).then((response: IResponse) => {
+	
+				const responseArray: [IList] = response.response				
+				
+				let synonymArray: string[] = []
+	
+				let prunedSynonymArray: string[] = []
+	
+				if(responseArray.length > 0) {
+					responseArray.forEach((list: IList) => {
+						list.list.synonyms.split('|').filter((synonym: string) => !(synonym.includes('(') || synonym.includes(' '))).forEach((synonym: string) => {
+							if(!synonymArray.includes(synonym)) {
+								synonymArray.splice(Math.floor(Math.random() * synonymArray.length), 0, synonym)
+							}
+						})
+					})
+				}
+	
+				console.log(`${word}: ${synonymArray.slice(0, 5)}`)
+			})
 		})
-
-		console.log(poemArray)
 	}
 
 	render(): JSX.Element {
